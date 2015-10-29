846229ccd3bda6171f7a80ede2044ad0cae75a8a
Kaische <Kaische.Gaming@gmail.com>
2015-10-29 08:22:55 +0100

    Created Server-Project

---
+++ Server/src/de/vCom/server/Program.java
@@ -0,0 +1,32 @@
+/**
+ * vCom is a chat service consisting of client and server application that encrypts every message send and thus allows a secure and for third parties not readable communication.
+ * Copyright (C) 2015 Kai Brandenbusch, Björn Lange, Christian Langer, Daniel Theis
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package de.vCom.server;
+
+/**
+ * Program entry class.
+ * @author Björn Lange
+ */
+public class Program {
+	/**
+	 * Program entry point.
+	 * @param args System arguments.
+	 */
+	public static void main(String[] args) {
+		System.out.println("Not yet implemented.");
+	}
+}
