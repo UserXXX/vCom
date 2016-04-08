36e992a584b97915d955a44c8e4481bbf3ff620a
UserXXX <anonym.nutzer@web.de>
2016-04-08 15:34:31 +0200

    Implemented BlocklistSync packet
    Implemented the BlocklistSync packet.

---
+++ Client/Desktop/src/main/java/de/vCom/client/desktop/presenter/packets/BlocklistSyncPacket.java
@@ -0,0 +1,61 @@
+/**
+ * vCom is a chat service consisting of client and server application that encrypts every message
+ * send and thus allows a secure and for third parties not readable communication.
+ * Copyright (C) 2016 Kai Brandenbusch, Björn Lange, Christian Langer, Daniel Theis
+ *
+ * This program is free software: you can redistribute it and/or modify it under the terms of the
+ * GNU General Public License as published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+ * without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See
+ * the GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along with this program.  If
+ * not, see <http://www.gnu.org/licenses/>.
+ */
+package de.vCom.client.desktop.presenter.packets;
+
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.util.List;
+
+import de.vCom.client.desktop.model.UserIdentifier;
+
+/**
+ * Packet for transferring the list of blocked users identifiers from server to client.
+ * @author Björn Lange
+ */
+public class BlocklistSyncPacket extends Packet {
+	/**
+	 * Protocol identifier for this packet.
+	 */
+	public static final int PROTOCOL_IDENTIFIER = 262;
+
+	private List<UserIdentifier> blocklist;
+	
+	/**
+	 * Gets the list of blocked users identifiers.
+	 * @return Gets the list of blocked users identifiers send by the server.
+	 */
+	public List<UserIdentifier> getBlocklist() {
+		return blocklist;
+	}
+	
+	/**
+	 * Creates a new BlocklistSyncPacket.
+	 */
+	public BlocklistSyncPacket() { }
+	
+	@Override
+	public void readFrom(DataInputStream input) throws IOException {
+		blocklist = readUserIdentifierList(input);
+	}
+
+	@Override
+	public void writeTo(DataOutputStream output) throws IOException {
+		throw new UnsupportedOperationException(
+				"Client is not allowed to send a BlocklistSyncPacket!");
+	}
+}
